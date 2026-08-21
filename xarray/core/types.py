d78502fddaacb33374dee5bbeb86a6d77e6e764d
YourCodingBench <snapshot@yourcodingbench.com>
2026-08-21 13:03:30 +0000

    ycb snapshot: orphan @ 6bb2b855498b5c68d7cca8cceb710365d58e6048

---
+++ xarray/core/types.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Literal, TypeVar, Union
+
+import numpy as np
+
+if TYPE_CHECKING:
+    from .common import DataWithCoords
+    from .dataarray import DataArray
+    from .dataset import Dataset
+    from .groupby import DataArrayGroupBy, GroupBy
+    from .indexes import Index
+    from .npcompat import ArrayLike
+    from .variable import Variable
+
+    try:
+        from dask.array import Array as DaskArray
+    except ImportError:
+        DaskArray = np.ndarray  # type: ignore
+
+
+T_Dataset = TypeVar("T_Dataset", bound="Dataset")
+T_DataArray = TypeVar("T_DataArray", bound="DataArray")
+T_Variable = TypeVar("T_Variable", bound="Variable")
+T_Index = TypeVar("T_Index", bound="Index")
+
+# Maybe we rename this to T_Data or something less Fortran-y?
+T_Xarray = TypeVar("T_Xarray", "DataArray", "Dataset")
+T_DataWithCoords = TypeVar("T_DataWithCoords", bound="DataWithCoords")
+
+ScalarOrArray = Union["ArrayLike", np.generic, np.ndarray, "DaskArray"]
+DsCompatible = Union["Dataset", "DataArray", "Variable", "GroupBy", "ScalarOrArray"]
+DaCompatible = Union["DataArray", "Variable", "DataArrayGroupBy", "ScalarOrArray"]
+VarCompatible = Union["Variable", "ScalarOrArray"]
+GroupByIncompatible = Union["Variable", "GroupBy"]
+
+ErrorChoice = Literal["raise", "ignore"]
+ErrorChoiceWithWarn = Literal["raise", "warn", "ignore"]
